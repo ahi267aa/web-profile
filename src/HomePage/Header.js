5cef67782be64dd4dc5233a24ee62d52d996e5dc
Lucy Chiu <ahi267aa@gmail.com>
2024-07-10 11:50:16 +0800

    fix(Home)

--- src/HomePage/Header.js
+++ src/HomePage/Header.js
@@ -11,10 +11,17 @@ function Header() {
       <div class="header-container">
         <div class="header-container-left">
           <div class="header-myinfo">
-            <h3>Hi I'm</h3>
-            <h1>Lucy Chiu</h1>
-            <h4>Software Engineer</h4>
-            <h5>2.5 years Software application development</h5>
+            <h3>Hi, I'm Lucy Chiu</h3>
+            <h2>Software Engineer</h2>
+            <link rel="stylesheet" href="styles.css"></link>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet"></link>
+            {/* <h5>2.5 years Software application development</h5> */}
+            <h5>
+              I love Programming and Architecture Design and have been persevering on my portfolio since 2019.
+            </h5>
+            <h5>
+              I can provide a effective assistance start right away!
+            </h5>
             <PersonalLink />
           </div>
         </div>
